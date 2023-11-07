680f5fe51eb5800b3537831fe47bb2f7658db422
suhan8984 <55016623+suhan8984@users.noreply.github.com>
2023-11-07 11:06:21 +0900

    Update ServerPacket.cpp

--- AutoResponse/ServerPacket.cpp
+++ AutoResponse/ServerPacket.cpp
@@ -49,9 +49,7 @@ void ServerPacket::EncodeFloat(float val) {
 	memcpy(&n, &val, sizeof(float));
 	sprintf_s(strTmp, "%08X", n);
 	std::string temp = std::string(strTmp);
-	std::string temp1 = temp.substr(0, 2) + temp.substr(2, 2);
-	std::string temp2 = temp.substr(4, 2) + temp.substr(6, 2);
-	Encode2((WORD)strtol(temp2.c_str(), NULL, 16));
-	Encode2((WORD)strtol(temp1.c_str(), NULL, 16));
-	// Encode4((DWORD)val);
+	Encode2((WORD)strtol(temp.substr(4, 4).c_str(), NULL, 16));
+	Encode2((WORD)strtol(temp.substr(0, 4).c_str(), NULL, 16));
+//	Encode4((DWORD)val);
 }
